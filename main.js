5d1d6281e964cc7467709e8dc5e2e72a1645a2fb
Dmitry <dmitrydohy@gmail.com>
2023-03-17 16:16:24 -0700

    Clear, delete, and operator buttons update screen
    Clear button clears screen.
    Delete button deletes last entry on screen.
    Operator buttons are added to screen when clicked. They don't appear if there are no numbers currently on the screen. Additionally, when the user attempts adding multiple operator symbols in a row, the last operator is instead replaced with the new one. This does not currently work when user attempts an equation more than 3 separate numbers long.
    Current js is pretty ugly; lots of nested if statements. Hoping to fix this later.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const add = function() {
     let sum = 0;
     for (i = 0; i < arguments.length; i++) {
       sum += arguments[i];
-      }
+    }
     return sum;
 };
  
@@ -21,20 +21,66 @@ const multiply = function() {
     return product;
 };
 
-
 const divide = function() {
 
 }
 */
 
-document.querySelector("#answers").innerHTML = "&nbsp";
+//declarations.
+let equation = "",
+    operators = "÷x-=+";
 
+//function that checks if the last character in a string contains any of the calculator's operators.
+function containsOperator(str) {
+    for (i = 0; i <= operators.length; i++) {
+        if (str.indexOf(operators[i]) > -1) {
+            return true;
+        }
+    }
+}
+
+//button clicking functions.
 const btns = document.querySelectorAll('.btn');
 
 for (const btn of btns) {
     btn.addEventListener("click",function calc(){
-        let equation = btn.innerHTML;
-        //if (equation == "")
-        document.querySelector("#answers").innerHTML = equation;
+        let clickedBtn = btn.innerHTML,
+            answers = document.querySelector("#answers"),
+            classes = btn.classList;
+        if (classes.contains("control")) {
+            if (clickedBtn == "clear") {
+                answers.innerHTML = "";
+                equation = "";
+            } else if (clickedBtn == "delete") {
+                equation = equation.substring(0, equation.length - 1);
+                answers.innerHTML = equation;
+            } else {
+                console.log(clickedBtn);
+            }
+        } else if (classes.contains("number")) {
+            if (equation == "") {
+                equation = clickedBtn;
+                answers.innerHTML = clickedBtn;
+            } else {
+                equation = equation + clickedBtn;
+                answers.innerHTML = equation;
+                }
+        } else if (classes.contains("operator")) {
+            if (equation == "") {
+                //add no operator
+            //if operator is in back of equation, replace operator with new button clicked.
+            } else if (containsOperator(equation) == true) {
+                equation = equation.substring(0, equation.length - 1);
+                equation = equation + clickedBtn;
+                answers.innerHTML = equation;
+            //if operator is anywhere else in equation, solve current equation, place answer in screen, then place new operator.
+            /*} else if (containsOperator(equation) != (equation.length - 1) && containsOperator(equation) > (equation.length - 1)) {
+                console.log("operator present");
+                */
+            } else {
+                equation = equation + clickedBtn;
+                answers.innerHTML = equation;
+            }
+        }
     });
 }
